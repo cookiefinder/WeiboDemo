ff917906f59ce5a0c87a299ed75fe84722a69c9d
Zijie Jiang <zijie.jiang@thoughtworks.com>
2020-05-11 12:18:06 +0800

    Initial Commit

---
+++ WeiboDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeiboDemo
+//
+//  Created by Zijie Jiang on 2020/5/11.
+//  Copyright © 2020 Zijie Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
