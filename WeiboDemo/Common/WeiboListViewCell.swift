e413010793db455e4ce387032dac9112dd80533a
yu.zhu1 <yu.zhu1@thoughtworks.com>
2020-05-14 14:39:58 +0800

    add weibo list view cell

---
+++ WeiboDemo/Common/WeiboListViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  WeiboListViewCell.swift
+//  WeiboDemo
+//
+//  Created by yu.zhu1 on 2020/5/14.
+//  Copyright © 2020 Zijie Jiang. All rights reserved.
+//
+
+import UIKit
+
+class WeiboListViewCell: UITableViewCell {
+
+    @IBOutlet weak var weiboTimeLineLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userScreenNameLabel: UILabel!
+    @IBOutlet weak var weiboContentLabel: UILabel!
+    
+    func configure() {}
+}
